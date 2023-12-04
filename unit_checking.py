293158fdccd36ae3e7ed9ed991540a2eccd7f741
Gnurro <knawurzelkopp@hotmail.de>
2023-12-04 19:47:07 +0100

    Add more feature-based retrieval functions: central claim unit first/last

--- unit_checking.py
+++ unit_checking.py
@@ -78,6 +78,81 @@ def get_pronoun_units():
     return pronoun_units
 
 
+"""
 pronoun_units = get_pronoun_units()
 for pronoun_unit in pronoun_units:
     print(pronoun_unit)
+"""
+
+# COMBINED FEATURES SETS
+
+
+def get_simple_set() -> set:
+    """
+    Get the set of texts that have no pronouns and no split units.
+    :return: Set of texts that have no pronouns and no split units.
+    """
+    all_texts_set = set(database.keys())
+    split_unit_texts = set()
+    for split_unit in get_split_units():
+        split_unit_texts.add(split_unit[0])
+    pronoun_unit_texts = set()
+    for pronoun_unit in get_pronoun_units():
+        pronoun_unit_texts.add(pronoun_unit[0])
+    bad_unit_texts = split_unit_texts.union(pronoun_unit_texts)
+    simple_set = all_texts_set.difference(bad_unit_texts)
+    return simple_set
+
+
+# print(get_simple_set())
+
+
+# LINEARIZATION STRATEGIES
+
+
+def get_central_first_texts() -> dict:
+    central_first_texts: dict = dict()
+    for inst_id, inst in database.items():
+        if inst['lin_strat'][0] == "c":
+            central_first_texts[inst_id] = inst
+    return central_first_texts
+
+
+"""
+for text_id, content in get_central_first_texts().items():
+    print(text_id, content)
+"""
+
+
+def get_central_last_texts() -> dict:
+    central_last_texts: dict = dict()
+    for inst_id, inst in database.items():
+        if inst['lin_strat'][-1] == "c":
+            central_last_texts[inst_id] = inst
+    return central_last_texts
+
+
+"""
+for text_id, content in get_central_last_texts().items():
+    print(text_id, content)
+"""
+
+# UTILITY
+
+
+def pretty_text(text_content):
+    text_units = text_content['units']
+    for text_unit in text_units:
+        print(text_unit['edu_id'], text_unit['text'])
+
+
+# pretty_text(database['b001'])
+
+
+def pretty_text_db(text_database: dict):
+    for text_id, content in text_database.items():
+        print(f"Text {text_id}")
+        pretty_text(content)
+
+
+pretty_text_db(get_central_first_texts())
